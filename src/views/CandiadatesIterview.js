24b5f7b7109221ecdafb9a917c97660af742b865
Mauricio Mendonca <mauricio.mendonca@zanc.com.br>
2019-10-30 13:48:33 -0300

    Adicionando componenente de dataTimePicker

--- src/views/CandiadatesIterview.js
+++ src/views/CandiadatesIterview.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react'
-import { Container, Row } from 'shards-react'
+import { Container, Row, Button } from 'shards-react'
+import DataTimePicker from  'react-datetime-picker';
+import { toastr } from 'react-redux-toastr'
 
 import PageTitle from '../components/common/PageTitle'
+import { day, month } from '../components/common/date/date'
 
 class CandidateInterview extends Component {
+  state = {
+    date: new Date()
+  }
+
+  onChange = date => this.setState({ date })
+
+  consisteDateTime = () => {
+    const date = new Date()
+    if(date < this.state.date){
+      const d = this.state.date.getDay()
+      const m = this.state.date.getMonth()
+      const h = `${this.state.date.getHours()}:${this.state.date.getMinutes()}`
+      toastr.info(`Entrevista agendada para dia ${d} (${day[d]}) de ${month[m]}, às ${h}`);
+    }else {
+      toastr.warning('A data para entrevista é inferior ou igual a de hoje');
+      this.setState({ date })
+    }
+  }
+
   render() {
     return(
       <Container fluid className="main-content-container px-4 pb-4">
@@ -11,6 +33,16 @@ class CandidateInterview extends Component {
           <PageTitle sm="4" title="Agendamento de Entrevistas" subtitle="Segunda Etapa" />
         </Row>
         <Row>
+          <DataTimePicker 
+            value={this.state.date}
+            onChange={this.onChange}
+          />
+          <Button 
+            theme="primary" 
+            onClick={() => this.consisteDateTime()}
+            className="mr-2"
+            ><i className="fas fa-check"></i>
+          </Button>
         </Row>
 	    </Container>
     )
